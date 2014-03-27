fa5e57551611b97d4c04acba4c843f6eed5d2b43
Johannes Nagl <johannes@nagl.name>
2014-03-27 15:11:24 +0100

    fixed daysAsSql once again
    tests are working again.

--- lib/Cake/Utility/CakeTime.php
+++ lib/Cake/Utility/CakeTime.php
@@ -439,10 +439,10 @@ class CakeTime {
  * @link http://book.cakephp.org/2.0/en/core-libraries/helpers/time.html#TimeHelper::daysAsSql
  */
 	public static function daysAsSql($begin, $end, $fieldName, $timezone = null) {
-		$begin = self::fromString($begin . ' 00:00:00', $timezone);
-		$end = self::fromString($end . ' 23:59:59', $timezone);
-		$begin = date('Y-m-d H:i:s', $begin);
-		$end = date('Y-m-d H:i:s', $end);
+		$begin = self::toServer($begin, $timezone);
+		$end = self::toServer($end, $timezone);
+		$begin = date('Y-m-d H:i:s', strtotime($begin));
+		$end = date('Y-m-d H:i:s', strtotime($end));
 
 		return "($fieldName >= '$begin') AND ($fieldName <= '$end')";
 	}
